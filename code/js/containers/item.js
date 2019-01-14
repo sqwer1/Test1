e1cf1a5b08871c58543af3d758f8820993e4be54
sqwer1 <44900035+sqwer1@users.noreply.github.com>
2019-01-14 19:56:46 +0700

    Add code/js/containers/item.js

---
+++ code/js/containers/item.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import axios from 'axios';
+import { itemsFetchData, select } from '../actions/index';
+
+class ItemList extends Component {
+    componentDidMount() {
+        this.props.fetchData('https://jsonplaceholder.typicode.com/photos');
+    }
+
+	showList () {
+		return this.props.items.map ((item) => {
+			return (
+				<div>
+					<div onClick={() => this.props.select (item)} key={item.id}>{item.id}{' '}{item.title}</div>
+				</div>
+			);
+		});
+	}
+	render () {
+		return (
+			<ol>
+				{this.showList ()}
+			</ol>
+		);
+	}
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.items
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(itemsFetchData(url)),
+	select: (item) => dispatch(select(item))
+    };
+};
+
+function matchDispatchToProps (dispatch) {
+	return bindActionCreators({select: select}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
