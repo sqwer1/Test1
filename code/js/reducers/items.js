12bfab41cf385a2ff21ff9b7c02e953cc45b7c2f
sqwer1 <44900035+sqwer1@users.noreply.github.com>
2019-01-14 19:59:17 +0700

    code/js/reducers/items.js

---
+++ code/js/reducers/items.js
@@ -0,0 +1,9 @@
+﻿export function items(state = [], action) {
+    switch (action.type) {
+        case 'ITEMS_FETCH_DATA_SUCCESS':
+            return action.items;
+
+        default:
+            return state;
+    }
+}
